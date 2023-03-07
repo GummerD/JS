30227917736231d58f89403132a544009cf0bb95
GummerD <denis_gummer@mail.ru>
2023-03-07 18:19:48 +0300

    Объявление функций, аргументы.

---
+++ js/function.js
@@ -0,0 +1,24 @@
+// функции:
+function info() {
+    console.log('Новая функиця');
+    console.log('Повторяем');
+}
+
+info();
+
+var string = prompt("Введите что-нибудь");
+
+function newInfo(string) {
+    alert('привет :' + string);
+
+}
+
+newInfo(string);
+
+function sum(a, b) {
+    console.log(a + b);
+}
+
+sum(5, 6);
+
+ 
